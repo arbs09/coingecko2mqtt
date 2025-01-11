a83a267843ad483bcc44631e024f632a9fbed09c
arbs09 <info@arbs09.de>
2025-01-11 23:43:11 +0100

    fix

--- app.py
+++ app.py
@@ -50,12 +50,10 @@ def publish(client):
             price, change24h = get_price(coin)
             if price is None or change24h is None:
                 continue
-            price_str = f"{price} {Corrency}"
-            change24h_str = f"{change24h}%"
             publish_topic = topic + "/" + coin
-            msg = f"{price_str} , 24h change: {change24h_str}"
-            result_price = client.publish(publish_topic + "/price", price_str)
-            result_24h_change = client.publish(publish_topic + "/24h_change", change24h_str)
+            msg = f"{price} , 24h change: {change24h}"
+            result_price = client.publish(publish_topic + "/price", price)
+            result_24h_change = client.publish(publish_topic + "/24h_change", change24h)
             status_price = result_price.rc
             status_24h_change = result_24h_change.rc
             if status_price == 0 and status_24h_change == 0:
